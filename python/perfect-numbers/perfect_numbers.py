14e9f06948d36124f6602f41c73f7b9b1f2fe475
Swarup <=>
2020-05-30 16:48:37 +0530

    Submitted the solution for perfect-numbers exercise withou passing the testcases involving exception messages.

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,17 @@
+
+def listOfFactors(n):
+	return [i for i in range(1,(n//2)+1) if n%i == 0]
+
+
 def classify(number):
-    pass
+	if number > 0:
+		ans_sum = sum(listOfFactors(number))
+		if ans_sum == number:
+			return "perfect"
+		if ans_sum > number:
+			return "abundant"
+		if ans_sum < number:
+			return "deficient"
+	else:
+		raise Exception('Number should be > 0.')
+		
